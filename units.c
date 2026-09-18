51f72b98effd639ac695fc6fc2b3210b1e23058c
runex-ai <sm8538998@gmail.com>
2026-09-18 11:53:55 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,27 @@
+/*
+AUTHOR:STEPHEN MWANGI
+REG NUMBER:BCS-05-0076/2026
+DESCRIPTION:UNITS_PROVIDED
+DATE:10/09/2026
+VERSION:1
+*/
+
+#include <stdio.h> //scanf(), printf()
+#include <conio.h>
+
+int main() {
+	//display units_provided
+	printf("Units Offered This Semester \n");
+	printf("1. BCE 111: COMPUTATIONAL LOGIC \n");
+	printf("2. BCU 111: COMMUNICATION SKILLS \n");
+	printf("3. BCU 112: DIGITAL LITERACY \n");
+	printf("4. BCU 113: HEALTH AND WELLNESS MANAGEMENT \n");
+	printf("5. BSD 111: STRUCTURED PROGRAMMING AND ALGORITHMS \n");
+	printf("6. MAT 111: BASIC MATHEMATICS \n");
+	printf("7. PHY 111: INTRODUCTION TO PHYSICS \n");
+	
+	return 0;  //execution successfull
+	
+	
+	
+}
